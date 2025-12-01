d165b458ea003e19173e254d169a480199b2ef2a
Zaalika-123 <u2552349@uel.ac.uk>
2025-12-01 23:45:16 +0000

    Update db.js

--- db.js
+++ db.js
@@ -6,11 +6,12 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('✅ MongoDB connected successfully');
+    console.log(' MongoDB connected successfully');
   } catch (error) {
-    console.error('❌ MongoDB connection failed:', error.message);
+    console.error(' MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
